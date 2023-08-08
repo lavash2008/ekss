cf164813473fd024b98deeee01f7f5bbf48963f0
lavash2008 <p-bayandin03@mail.ru>
2023-08-08 20:53:50 +0500

    new file:   fonts/ice.otf 	modified:   pages/home.php 	modified:   styles/home.css 	modified:   styles/index.css 	modified:   templates/home.html 	modified:   templates/partPage/footer.html 	modified:   templates/partPage/head.html

--- pages/home.php
+++ pages/home.php
@@ -13,5 +13,5 @@ $twig = new \Twig\Environment($loader, [
 $resultStudents= $mysqli->query(sprintf("SELECT `SLastName` as 'lastName',`SFirstName` as 'name',`SMindName` as 'mindName',`SBirthDate` as 'birthDate', CONCAT(`classes`.`CLevel`,`classes`.`Cletter`) as 'class' FROM `students`,`classes` WHERE `students`.`CId`=`classes`.`CId` order by `SBirthDate` DESC"));
 
 //загрузка шаблона
-echo $twig->render('home.html', ['title'=>'Главная', 'style'=>'authorization', 'result'=>$resultStudents]);
+echo $twig->render('home.html', ['title'=>'Главная', 'style'=>'home', 'result'=>$resultStudents]);
 ?>
